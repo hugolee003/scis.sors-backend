36e812ee0542ba0e4674648aac9a4926c9b68be7
Ugochukwu Egeonu <jamesegeonu003@gmail.com>
2023-07-09 13:32:01 +0100

    commit

--- Backend/build/config/production.js
+++ Backend/build/config/production.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     port: 5000,
-    dbUri: "mongodb+srv://Godsent-Michael:Michy1253@cluster0.h2nsryw.mongodb.net/Scissor?retryWrites=true&w=majority"
+    dbUri: "process.env.MONGO_URI"
 };
